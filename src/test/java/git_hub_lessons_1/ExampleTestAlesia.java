212d12686d187a617457ce4f5353e890346abd03
Alesia Abramova <alesja.abramova@gmail.com>
2022-09-16 11:42:57 +0200

    added second test

--- src/test/java/git_hub_lessons_1/ExampleTestAlesia.java
+++ src/test/java/git_hub_lessons_1/ExampleTestAlesia.java
@@ -5,8 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class ExampleTestAlesia {
     @Test
-    void exampleTestAlesia() {
+    void exampleTestAlesia0() {
         Assertions.assertTrue(2 > 1);
 
+    }
+    @Test
+    void exampleTestAlesia1() {
+        Assertions.assertTrue(3 > 1);
     }
 }
