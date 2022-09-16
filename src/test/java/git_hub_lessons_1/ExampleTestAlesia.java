b156f0ba858e79347dd3ce612347ec553504526e
Alesia Abramova <alesja.abramova@gmail.com>
2022-09-16 11:14:13 +0200

    Init commit from terminal

---
+++ src/test/java/git_hub_lessons_1/ExampleTestAlesia.java
@@ -0,0 +1,12 @@
+package git_hub_lessons_1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTestAlesia {
+    @Test
+    void exampleTestAlesia() {
+        Assertions.assertTrue(2 > 1);
+
+    }
+}
