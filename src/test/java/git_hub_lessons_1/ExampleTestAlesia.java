c3c7bc9a7ef65907a86357c4959efc6b768a5fda
Alesia Abramova <alesja.abramova@gmail.com>
2022-09-19 11:15:32 +0200

    fix_logic_conflict2

--- src/test/java/git_hub_lessons_1/ExampleTestAlesia.java
+++ src/test/java/git_hub_lessons_1/ExampleTestAlesia.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 public class ExampleTestAlesia {
     @Test
-    void exampleTestAlesia() {
+    void exampleTestAlesiaConflict() {
         Assertions.assertTrue(2 >= 0);
 
     }
